bbdbe15582ce7a9745c051b0cda0bbc1c436c53f
Singhranashivang <ranashivang567.com>
2026-03-17 02:05:45 +0530

    refactor referral component to use utility

--- components/Referral.tsx
+++ components/Referral.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import { useState } from "react";
+import { generateReferralCode } from "../utils/generateReferral";
+
 
 export default function Referral() {
 
   const [referralLink, setReferralLink] = useState("");
 
-  function generateReferral() {
-    const link = window.location.origin + "?ref=" + Math.random().toString(36).substring(7);
-    setReferralLink(link);
-  }
+
+ function generateReferral() {
+  const link = window.location.origin + "?ref=" + generateReferralCode();
+  setReferralLink(link);
+}
 
   return (
     <div>
