7a00c063688791eed979a312426f26dd7288092a
Singhranashivang <ranashivang567.com>
2026-03-17 01:51:33 +0530

    display wallet connect on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import WalletConnect from "../components/WalletConnect";
+
+
 export default function Home() {
   return (
     <main>
       <h1>Base Airdrop Dashboard</h1>
+      <WalletConnect />
     </main>
   );
 }
