ecd94ab572bd31fbf6eed16e8fcab27ec3392c1f
Singhranashivang <ranashivang567.com>
2026-03-22 11:22:17 +0530

    connect leaderboard to api

--- components/Leaderboard.tsx
+++ components/Leaderboard.tsx
@@ -1,20 +1,24 @@
 "use client";
 
-const users = [
-  { name: "Rana", points: 120 },
-  { name: "User2", points: 95 },
-  { name: "User3", points: 80 }
-];
+import { useEffect, useState } from "react";
 
 export default function Leaderboard() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/leaderboard")
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
   return (
     <div>
-        <div style={{ border: "1px solid gray", padding: "10px" }}></div>
       <h2>Leaderboard</h2>
 
-      {users.map((user, index) => (
+      {users.map((user: any, index) => (
         <p key={index}>
-          {user.name} - {user.points} pts
+          {user.name} - {user.points}
         </p>
       ))}
     </div>
