c9fdad450144dff56e0f491aac0a0d89c325b034
Singhranashivang <ranashivang567.com>
2026-03-22 11:20:04 +0530

    add dummy leaderboard data

--- components/Leaderboard.tsx
+++ components/Leaderboard.tsx
@@ -1,9 +1,21 @@
 "use client";
 
+const users = [
+  { name: "Rana", points: 120 },
+  { name: "User2", points: 95 },
+  { name: "User3", points: 80 }
+];
+
 export default function Leaderboard() {
   return (
     <div>
       <h2>Leaderboard</h2>
+
+      {users.map((user, index) => (
+        <p key={index}>
+          {user.name} - {user.points} pts
+        </p>
+      ))}
     </div>
   );
 }
